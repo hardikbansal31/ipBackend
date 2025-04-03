e73bc7c9c7bbdb3d2d55eab1735da6e651712ec1
Hardik <hardik@Neerajs-MacBook-Air.local>
2025-04-03 20:46:20 +0530

    vercel config

--- src/main/java/com/task/taskagain/config/SecurityConfig.java
+++ src/main/java/com/task/taskagain/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // ✅ Allow frontend
+        configuration.setAllowedOrigins(List.of("https://ip-gga6a8mqm-hardikbansal31s-projects.vercel.app/")); // ✅ Allow frontend
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // ✅ Allow cookies & auth headers
