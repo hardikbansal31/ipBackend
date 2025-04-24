f7e6e7fae8cba0ac73400c8d8b9513b6c9a2e35d
Hardik <hardik@Neerajs-MacBook-Air.local>
2025-04-24 18:22:36 +0530

    cors

--- src/main/java/com/task/taskagain/config/SecurityConfig.java
+++ src/main/java/com/task/taskagain/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://ip-mocha-nine.vercel.app/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://ip-mocha-nine.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
